a3bd5447f3cb17ffdc924b760c68b56619d2a410
priamitra12 <priamitra12>
2022-03-01 01:13:57 +0700

    penyelesaian latihan pertemuan 3

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -5,19 +5,22 @@
 // do.. while
 
 
-// for( $i = 0; $i < 4; $i++ ) {
-//     echo "Hello World! <br>";
-// }
+for( $i = 0; $i < 4; $i++ ) {
+    echo "Hello World! <br>";
+}
 
-// $i =0 ;
-// while ( $i < 6 ) {
-//     echo "Hello World! <br>";
-// $i++;
-// }
+$i =0 ;
+while ( $i < 6 ) {
+    echo "Hello World! <br>";
+$i++;
+}
 
 $i = 0;
 do {
     echo "Hello World! <br>";
     $i++;
 } while ( $i < 5 );
+
+
+
 ?>
